94a2c303e7c43c8dbd27173106aa64b0dc9805fe
seiku2000 <eslumter@gmail.com>
2026-09-19 11:40:24 -0700

    resourceTool

--- src/app/country/components/country-table/country-table.ts
+++ src/app/country/components/country-table/country-table.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Country } from '../../interfaces/data-country.interface';
 import { Countrys } from '../../interfaces/country.interfacae';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-country-table',
-  imports: [],
+  imports: [DecimalPipe],//para utilizar el pipe number
   templateUrl: './country-table.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, resource, signal } from '@angular/core';
 import { CountryInputSearch } from '../../components/country-input-search/country-input-search';
 import { CountryTable } from '../../components/country-table/country-table';
 import { CountryService } from '../../services/country.service';
 //import { Country, DataCountry } from '../../interfaces/data-country.interface';
 import { Countrys } from '../../interfaces/country.interfacae';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -12,32 +13,65 @@ import { Countrys } from '../../interfaces/country.interfacae';
   //changeDetection: ChangeDetectionStrategy.Eager,
 })
 export class ByCapitalPage {
-
-
   public searchCarpitalService = inject(CountryService);
-  isLoading = signal<boolean>(false);
-  isError = signal<string | null>(null);
-  countries = signal<Countrys[]>([]);
+  query = signal<string>('');
+  emptyError = signal<string | null>(null);
 
+  countryRsource = resource({
+    params: () => ({ query: this.query() }),
+    loader: async ({ params }) => {
+      const { query } = params;
+      if (!query) return [];
 
+      return await firstValueFrom(this.searchCarpitalService.searchByCapital(query));
+    }
+  });
 
   onSearch(value: string) {
+    const trimmed = value.trim();
 
-    if (this.isLoading()) return;
-    this.isLoading.set(true);
-    this.isError.set(null);
+    if (!trimmed) {
+      this.emptyError.set('Debes ingresar el nombre de una capital para buscar');
+      return;
+    }
 
-
-    console.log(value);
-    this.searchCarpitalService.searchByCapital(value).subscribe((data) => {
-      console.log(data);
-      this.isLoading.set(false);
-      this.countries.set(data);
-
-    });
-
+    this.emptyError.set(null);
+    this.query.set(trimmed);
   }
 
+  /*
+    public searchCarpitalService = inject(CountryService);
+    isLoading = signal<boolean>(false);
+    isError = signal<string | null>(null);
+    countries = signal<Countrys[]>([]);
+  
+  
+  
+    onSearch(value: string) {
+  
+      if (this.isLoading()) return;
+      this.isLoading.set(true);
+      this.isError.set(null);
+  
+  
+      console.log(value);
+      this.searchCarpitalService.searchByCapital(value).subscribe({
+        next: (countries) => {
+          //console.log(countries);
+          this.isLoading.set(false);
+          this.countries.set(countries);
+        },
+        error: (error) => {
+          console.log(error);
+          this.isLoading.set(false);
+          this.countries.set([]);
+          this.isError.set(error);
+  
+        }
+      })
+  
+    }
+  */
 
 
 
